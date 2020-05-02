9b5b19fda9806b7dda677219c3467185d6f26add
mranch <anchiforov@gmail.com>
2020-05-03 00:35:59 +0300

    graph 8 in progress

--- main.py
+++ main.py
@@ -44,15 +44,30 @@ def build_corr_by_day_range(arr, day_range):
 
 def build_3d_graph_6(arr, C_dist):
     z = [h.get_height() for h in C_dist.patches]
-    print(f"z = {z}")
-    y = np.linspace(1, len(arr[0]), len(z))
-    x = np.linspace(0, 1, len(z))
+    y = list(np.linspace(1, len(arr[0]), len(z)))
+    x = list(np.linspace(0, 1, len(z)))
     X, Y = np.meshgrid(x, y)
     Z, _ = np.meshgrid(z, z)
     fig = plt.figure()
     ax = plt.axes(projection="3d")
-    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="plasma", edgecolor="none")
+    # surf = ax.plot_trisurf(x, y, z, cmap="viridis")
     fig.colorbar(surf)
+
+
+def build_graph_8(C, q):
+    def get_prm_lambda(l):
+        return (q/2*np.pi)*(np.sqrt((lambda_plus-l)*(l - lambda_minus))/l)
+
+    eig = np.linalg.eigvals(C)
+    lambda_plus = 1 + (1/q) + 2*np.sqrt(1/q)
+    lambda_minus = 1 + (1/q) - 2*np.sqrt(1/q)
+    print(eig)
+    prm = [get_prm_lambda(l) for l in eig]
+    print(prm)
+    sns.distplot(eig)
+    sns.distplot(prm)
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -68,16 +83,21 @@ if __name__ == "__main__":
     C_mixed = build_corr_matrix(arr_mixed)
 
     # Heat maps
-    show_heat_map(C)
-    show_heat_map(C_mixed)
-
-    # Show distribution plots with graph 6 -> normal matrix first and then the mixed one
-    C_dist = sns.distplot(C)
-    plt.show()
-    build_3d_graph_6(arr, C_dist)
-    plt.show()
-
-    C_mixed_dist = sns.distplot(C_mixed)
-    plt.show()
-    build_3d_graph_6(arr_mixed, C_mixed_dist)
-    plt.show()
+    # show_heat_map(C)
+    # show_heat_map(C_mixed)
+    #
+    # # Show distribution plots with graph 6 -> normal matrix first and then the mixed one
+    #
+    # # dist plot + 3d for usual matrix
+    # C_dist = sns.distplot(C)
+    # plt.show()
+    # build_3d_graph_6(arr, C_dist)
+    # plt.show()
+    #
+    # # dist plot + 3d for mixed matrix
+    # C_mixed_dist = sns.distplot(C_mixed)
+    # plt.show()
+    # build_3d_graph_6(arr_mixed, C_mixed_dist)
+    # plt.show()
+    q = len(arr[0]) / len(arr)
+    build_graph_8(C, q)
